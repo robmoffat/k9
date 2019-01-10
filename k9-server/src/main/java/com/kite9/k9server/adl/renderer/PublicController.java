e6c1e685cf81e9c1cc9fedc89f0e257d1091b7e9
Rob Moffat <robmoffat@mac.com>
2019-01-10 16:45:06 +0000

    POC for SVG behaviours

---
+++ k9-server/src/main/java/com/kite9/k9server/adl/renderer/PublicController.java
@@ -0,0 +1,59 @@
+package com.kite9.k9server.adl.renderer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.Charset;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.kite9.k9server.adl.format.media.MediaTypes;
+import com.kite9.k9server.adl.holder.ADL;
+import com.kite9.k9server.adl.holder.ADLImpl;
+
+/**
+ * Returns content held within the public folder of the project, which anyone can access.
+ * 
+ * @author robmoffat
+ *
+ */
+@Controller()
+public class PublicController {
+	
+	@GetMapping(path="/public/**/*.html", produces=MediaType.TEXT_HTML_VALUE)
+	public @ResponseBody ADL loadStaticHtml(HttpServletRequest request) throws IOException {
+		String url = request.getRequestURL().toString();
+		String stub = url.substring(url.indexOf("/public/")+8, url.lastIndexOf(".html"));
+		String resourceName = "/static/public/"+stub;
+		String xml = loadXML(resourceName);
+		return new ADLImpl(xml, url);
+	}
+	
+	@GetMapping(path="/public/**/*.svg", produces=MediaTypes.SVG_VALUE)
+	public @ResponseBody ADL loadStaticSvg(HttpServletRequest request) throws IOException {
+		String url = request.getRequestURL().toString();
+		String stub = url.substring(url.indexOf("/public/")+8, url.lastIndexOf(".svg"));
+		String resourceName = "/static/public/"+stub;
+		String xml = loadXML(resourceName);
+		return new ADLImpl(xml, url);
+	}
+	
+	private String loadXML(String resourceName) throws IOException {
+		InputStream resourceAsStream = this.getClass().getResourceAsStream(resourceName);
+		
+		if (resourceAsStream == null) {
+			resourceAsStream = this.getClass().getResourceAsStream(resourceName+ ".xml");
+		} 
+		
+		if (resourceAsStream == null) {
+			resourceAsStream = this.getClass().getResourceAsStream(resourceName+ "/index.xml");
+		}
+		return StreamUtils.copyToString(resourceAsStream, Charset.defaultCharset());
+	}
+	
+}
---
+++ k9-server/src/main/resources/static/public/scripts/hover.js
@@ -0,0 +1,26 @@
+window.addEventListener('load', function() {
+    console.log('All assets are loaded')
+    
+    var selectedElement;
+    
+    document.querySelectorAll(".hover").forEach(function(v) {
+		var classes = v.classList;
+    	v.addEventListener("mouseover", function() {
+    		if (!classes.contains("mouseover")) {
+    			classes.add("mouseover");
+    			
+    			if (selectedElement != undefined) {
+    				selectedElement.classList.remove("mouseover")
+    				selectedElement = v;
+    			}
+    		 }
+    	})
+    	
+    	v.addEventListener("mouseout", function() {
+    		classes.remove("mouseover");
+    		if (selectedElement == v) {
+    			selectedElement = undefined;
+    		}
+    	})
+    })
+})
